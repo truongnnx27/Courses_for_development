07033e0e7bc19054513c5296b7d8dbeac8b46407
Nguyễn Quốc Khánh <nguyenkhanhahihivl@gmail.com>
2024-10-01 10:15:15 +0700

    commentApi

--- src/main/java/com/example/coursefordevelopment/api/commentApi.java
+++ src/main/java/com/example/coursefordevelopment/api/commentApi.java
@@ -1,11 +1,24 @@
 package com.example.coursefordevelopment.api;
 
+import com.example.coursefordevelopment.entity.Comment;
+import com.example.coursefordevelopment.reponsitory.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin("*")
 public class commentApi {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @GetMapping("/getallComment")
+    public List<Comment> getComments() {
+        return commentRepository.findAll();
+    }
     
 }
